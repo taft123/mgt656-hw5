91518dc7467b00aecbc92a8c338374c6dc113e18
Guangkun <taft123@gmail.com>
2016-10-07 01:06:14 +0000

    Taft add nickname

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am taft from MAM 2017');
+});
+
+app.get('/nickname', function(request, response){
+    response.send("I am 'poised-stingray' from MAM 2017");
 });
 
 app.listen(process.env.PORT || 4000);
